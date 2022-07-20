7d580158861234e8bb798341a656aa980c3f2a2f
alpha-oss <monish.shetty770@gmail.com>
2022-07-20 19:42:45 +0530

    fixed home

--- app/src/main/java/com/example/messhall/MainActivity.java
+++ app/src/main/java/com/example/messhall/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,16 +23,20 @@ public class MainActivity extends AppCompatActivity {
         logBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,signin.class);
-                startActivity(intent);
+                try {
+                    Intent intent = new Intent(getBaseContext(),signin.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
 
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,signup.class);
-                startActivity(intent);
+                Intent intent2 = new Intent(MainActivity.this,signup.class);
+                startActivity(intent2);
             }
         });
 
--- app/src/main/java/com/example/messhall/signin.java
+++ app/src/main/java/com/example/messhall/signin.java
@@ -2,13 +2,44 @@ package com.example.messhall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class signin extends AppCompatActivity {
+
+    EditText uname , pass;
+    Button signinBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        uname = findViewById(R.id.uname);
+        pass = findViewById(R.id.pass);
+        signinBtn = findViewById(R.id.signinBtn);
+
+        signinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String udata = uname.getText().toString();
+                String pdata = pass.getText().toString();
+
+                if(udata=="admin" && pdata=="admin"){
+                    Intent intent = new Intent(signin.this,admin_dashboard.class);
+                    startActivity(intent);
+
+                }
+                else{
+                    Toast.makeText(signin.this,"invalid credentials",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
