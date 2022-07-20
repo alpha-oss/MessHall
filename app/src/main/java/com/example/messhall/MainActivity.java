0d0f01e29487d2239ded942d586228c6f3fc507f
alpha-oss <monish.shetty770@gmail.com>
2022-07-20 12:41:18 +0530

    updated home

--- app/src/main/java/com/example/messhall/MainActivity.java
+++ app/src/main/java/com/example/messhall/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.messhall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button logBtn , regBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logBtn = findViewById(R.id.logBtn);
+        regBtn = findViewById(R.id.regBtn);
+
+        logBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,signin.class);
+                startActivity(intent);
+            }
+        });
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,signup.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
