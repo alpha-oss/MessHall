1adbd1a3d4e195b94ab27dde06e473fd70501529
wickedrock1 <vamsi0098@gmail.com>
2022-07-28 21:19:51 +0530

    final change

--- app/src/main/java/com/example/messhall/feedback.java
+++ app/src/main/java/com/example/messhall/feedback.java
@@ -1,7 +1,9 @@
 package com.example.messhall;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,13 +12,27 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class feedback extends AppCompatActivity {
 
-    RadioButton radioButton1,radioButton2,radioButton3,radioButton4,radioButton5,radioBtn1,radioBtn2,radioBtn3,radioBtn4,radioBtn5;
-    EditText feedText;
-    RadioGroup radioGroup;
-    RadioGroup radioGroup1;
+
+    EditText feedText,feed1,feed2;
     Button submitBtn;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,99 +42,66 @@ public class feedback extends AppCompatActivity {
         submitBtn = findViewById(R.id.submitBtn);
 
         feedText=findViewById(R.id.feedText);
-
-        radioBtn1=findViewById(R.id.radioBtn1);
-        radioBtn2=findViewById(R.id.radioBtn2);
-        radioBtn3=findViewById(R.id.radioBtn3);
-        radioBtn4=findViewById(R.id.radioBtn4);
-        radioBtn5=findViewById(R.id.radioBtn5);
+        feed1=findViewById(R.id.feed1);
+        feed2=findViewById(R.id.feed2);
 
 
-        radioGroup = (RadioGroup) findViewById(R.id.radioGrp);
-        radioGroup1=(RadioGroup) findViewById(R.id.radioGrp1);
-        radioButton1=findViewById(R.id.radioButton1);
-        radioButton2=findViewById(R.id.radioButton2);
-        radioButton3=findViewById(R.id.radioButton3);
-        radioButton4=findViewById(R.id.radioButton4);
-        radioButton5=findViewById(R.id.radioButton5);
-
-        radioButton1.setOnClickListener(new View.OnClickListener() {
+        submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (radioGroup.getCheckedRadioButtonId() == R.id.radioButton1)
-                {
-                    Toast.makeText(feedback.this,"NICE", Toast.LENGTH_SHORT).show();
 
-                }
+                String feedd1= feed1.getText().toString();
+                String feedd2= feed2.getText().toString();
+                String feedtxt=feedText.getText().toString();
+
+                FirebaseAuth mAuth;
+                mAuth = FirebaseAuth.getInstance();
+                FirebaseUser user = mAuth.getCurrentUser();
+                Map<String, Object> city = new HashMap<>();
+                city.put("Feedback1", feedd1);
+                city.put("Feedback2", feedd2);
+                city.put("Feedback3", feedtxt);
+
+                DocumentReference docRef = db.collection("users").document(user.getUid());
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                String ss = "";
+
+                                ss = document.getData().get("name").toString();
+
+
+                                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                DocumentReference docRef =db.collection("feedback").document("feed");
+                                docRef.update(ss, FieldValue.arrayUnion(city));
+
+//                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+//                        Toast.makeText(breakfast.this,document.getData()["Breakfast"][0].toString(),Toast.LENGTH_LONG).show();
+                            } else {
+//                        Log.d(TAG, "No such document");
+                            }
+                        } else {
+//                    Log.d(TAG, "get failed with ", task.getException());
+                        }
+//                        Intent intent = new Intent(breakfast.this,menuPage.class);
+//                        startActivity(intent);
+                    }
+
+
+
+
+                });
+
 
 
 
             }
         });
 
-//        else if(radioBtn2.isSelected())
-//        {
-//            radioBtn1.setEnabled(false);
-//            radioBtn3.setEnabled(false);
-//            radioBtn4.setEnabled(false);
-//            radioBtn5.setEnabled(false);
-//        }
-//        else if(radioBtn3.isSelected())
-//        {
-//            radioBtn2.setEnabled(false);
-//            radioBtn1.setEnabled(false);
-//            radioBtn4.setEnabled(false);
-//            radioBtn5.setEnabled(false);
-//        }
-//        else if(radioBtn4.isSelected())
-//        {
-//            radioBtn2.setEnabled(false);
-//            radioBtn3.setEnabled(false);
-//            radioBtn1.setEnabled(false);
-//            radioBtn5.setEnabled(false);
-//        }
-//        else if(radioBtn5.isSelected())
-//        {
-//            radioBtn2.setEnabled(false);
-//            radioBtn3.setEnabled(false);
-//            radioBtn4.setEnabled(false);
-//            radioBtn1.setEnabled(false);
-//        }
-//        else if(radioButton1.isSelected())
-//        {
-//            radioButton2.setEnabled(false);
-//            radioButton3.setEnabled(false);
-//            radioButton4.setEnabled(false);
-//            radioButton5.setEnabled(false);
-//        }
-//        else if(radioButton2.isSelected())
-//        {
-//            radioButton1.setEnabled(false);
-//            radioButton3.setEnabled(false);
-//            radioButton4.setEnabled(false);
-//            radioButton5.setEnabled(false);
-//        }
-//        else if(radioButton3.isSelected())
-//        {
-//            radioButton2.setEnabled(false);
-//            radioButton1.setEnabled(false);
-//            radioButton4.setEnabled(false);
-//            radioButton5.setEnabled(false);
-//        }
-//        else if(radioButton4.isSelected())
-//        {
-//            radioButton2.setEnabled(false);
-//            radioButton3.setEnabled(false);
-//            radioButton1.setEnabled(false);
-//            radioButton5.setEnabled(false);
-//        }
-//        else if(radioButton5.isSelected())
-//        {
-//            radioButton2.setEnabled(false);
-//            radioButton3.setEnabled(false);
-//            radioButton4.setEnabled(false);
-//            radioButton1.setEnabled(false);
-//        }
+
 
 
 
--- app/src/main/java/com/example/messhall/homeuser.java
+++ app/src/main/java/com/example/messhall/homeuser.java
@@ -2,6 +2,7 @@ package com.example.messhall;
 
 import static java.time.LocalDate.now;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,7 +14,14 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -24,7 +32,9 @@ public class homeuser extends AppCompatActivity {
     ImageButton menuBtn,feedBtn;
     Button logout;
     TextView date1;
+    EditText userName;
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     @Override
@@ -36,9 +46,57 @@ public class homeuser extends AppCompatActivity {
 
         feedBtn=(ImageButton)findViewById(R.id.feedBtn);
         menuBtn=(ImageButton) findViewById(R.id.mnuBtn);
+        userName=findViewById(R.id.userName);
         logout=(Button)findViewById(R.id.logout);
         date1=findViewById(R.id.date);
         date1.setText(java.time.LocalDate.now().toString());
+
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+
+
+        DocumentReference docRef = db.collection("users").document(user.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String ss = "";
+
+                        ss = document.getData().get("name").toString();
+                        userName.setText(ss);
+
+//                                Map<String, Object> city = new HashMap<>();
+//                                city.put(ar,ss);
+
+//                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//                        DocumentReference docRef =db.collection("cities").document("BJ");
+//                        docRef.update("name", FieldValue.arrayUnion(ss));
+//                        Intent intent = new Intent(homeuser.this,menuPage.class);
+//                        startActivity(intent);
+
+
+
+//                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+//                        Toast.makeText(breakfast.this,document.getData()["Breakfast"][0].toString(),Toast.LENGTH_LONG).show();
+                    } else {
+//                        Log.d(TAG, "No such document");
+                    }
+                } else {
+//                    Log.d(TAG, "get failed with ", task.getException());
+                }
+//                        Intent intent = new Intent(breakfast.this,menuPage.class);
+//                        startActivity(intent);
+            }
+
+
+
+
+        });
 
         menuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
